a23e3ff3cc176faf25dab15c71cf6cb484468117
Luckas Fonseca <lukyta246@gmail.com>
2023-01-10 19:59:25 -0300

    second

--- banco.sql
+++ banco.sql
@@ -1,2 +1,7 @@
-create table products
-inserts (nome, preco)
+create table products (
+  id INTEGER,
+  nome VARCHAR(30),
+  preco MONEY
+);
+INSERT INTO products (nome, preco)
+  VALUES ("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),( "id":null,"nome":"requeijao","preco":"R$ 5,00"),("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),("id":null,"nome":"requeijao","preco":"R$ 5,00");
