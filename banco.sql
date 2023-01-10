2f1def3ecf156341b771afe926b0d455ff7f898d
Andre Fernandes <andre@vertigo.com.br>
2023-01-10 20:40:58 -0300

    ajustes sql

--- banco.sql
+++ banco.sql
@@ -1,7 +1,13 @@
-create table products (
+-- Active: 1673393656117@@127.0.0.1@5432@mercado
+create table IF NOT EXISTS products (
   id INTEGER,
   nome VARCHAR(30),
   preco MONEY
 );
-INSERT INTO products (nome, preco)
-  VALUES ("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),( "id":null,"nome":"requeijao","preco":"R$ 5,00"),("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),("id":null,"nome":"requeijao","preco":"R$ 5,00");
+
+INSERT INTO products (id, nome, preco) VALUES
+  (1,'bisnaguinha',10.00);
+
+--  ( "id":null,"nome":"requeijao","preco":"R$ 5,00"),
+--  ("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),
+--  ("id":null,"nome":"requeijao","preco":"R$ 5,00");
