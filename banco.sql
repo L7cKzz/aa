e8a6dbef07d20646cd57f2d04d332908e60f94a9
Andre Fernandes <andre@vertigo.com.br>
2023-01-10 20:45:41 -0300

    ajustes sql

--- banco.sql
+++ banco.sql
@@ -1,9 +1,16 @@
 -- Active: 1673393656117@@127.0.0.1@5432@mercado
+
+-- faça o campo ID ser um autoincremento e nao apenas um inteiro
+-- faça o campo ID ser uma chave primária
+-- isso irá exigir que os INSERTs ignorem esse campo
+
 create table IF NOT EXISTS products (
   id INTEGER,
   nome VARCHAR(30),
   preco MONEY
 );
+
+delete from products;
 
 INSERT INTO products (id, nome, preco) VALUES
   (1,'bisnaguinha',10.00);
