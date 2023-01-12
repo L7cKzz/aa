a95c699183b0215f56bac2c1d272a9b350fa54ca
Luckas Fonseca <lukyta246@gmail.com>
2023-01-12 19:45:08 -0300

    third

--- banco.sql
+++ banco.sql
@@ -1,7 +1,7 @@
 create table products (
-  id INTEGER,
+  id SERIAL PRIMARY KEY,
   nome VARCHAR(30),
   preco MONEY
 );
 INSERT INTO products (nome, preco)
-  VALUES ("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),( "id":null,"nome":"requeijao","preco":"R$ 5,00"),("id":null,"nome":"bisnaguinha","preco":"R$ 10,00"),("id":null,"nome":"requeijao","preco":"R$ 5,00");
+VALUES ('bisnaguinha', 10,00'), ('requeijao', '7,50');
